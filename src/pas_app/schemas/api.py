b9e47ea8af091173cec97759ca21258ad0ca00a9
Vlad <vovanikolaev140707@gmail.com>
2026-04-26 18:12:35 +0300

    added JSON responses classes from server

--- src/pas_app/schemas/api.py
+++ src/pas_app/schemas/api.py
@@ -5,4 +5,15 @@ class Login_RegisterRequest(BaseModel):
     username: str
     password: str
     
+#===================================
+#           Responses
+#===================================
+class MessageResponse(BaseModel):
+    ok: bool
+    message: str
+    
+class LoginResponse(MessageResponse):
+    access_token: str
+    token_type: str
+    
 
